23e5e09352d988e1cde44e5536ccd39584b35645
Pontus Tahir <pontus.tahir@gmail.com>
2020-03-19 21:29:58 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Button } from 'antd'
 import "./App.css";
 import pic from './tres_commas.jpg'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <h1>design-system-app</h1>
       <h3>Trés Commás</h3>
+      <Button type='primary'>Buy Me!</Button>
       <img src={pic} alt="Trés Commás"/>
     </div>
   );
