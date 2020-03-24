a103d88b99a36464e9d2cff732fe026093ad1971
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-03-24 19:18:24 +0100

    intro img and text done

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -9,54 +9,61 @@ class StartPage extends React.Component<Props, State> {
   render() {
     return (
       <div>
-        <Row justify="center">
-          <Col span={24}>Intro Img</Col>
+        <Row>
+          <Col xs={{ span: 24 }} md={{ span: 20, offset: 2 }}>
+            <img
+              style={{
+                objectFit: "cover",
+                objectPosition: "50% 80%",
+                height: "25rem",
+                width: "100%"
+              }}
+              src="https://www.auro.se/fileadmin/_processed_/csm_Varmgul_Anne_Jeppson_e0200f7f6e.jpg"
+            ></img>
+          </Col>
         </Row>
-        <Row justify="center">
-          <Col span={24}>Intro Text</Col>
+        <Row style={{ textAlign: "center"}}>
+          <Col span={20} offset={2}>
+            Mini Colorini är tillverkare av ekologiska färger och
+            ytbehandlingar. Vi utvecklar, producerar och levererar ekologisk
+            färg och ytbehandlingsprodukter med ett genomgående
+            hållbarhetsperspektiv och certifierad koldioxidneutral tillverkning.
+            Färgen framställs av biologiskt nedbrytbarbra råvaror: växtextrakt,
+            krita, mineraler och jordpigment är komposterbara och har ingen
+            negativ miljöpåverkan. Certifierat VOC-fri. Resultatet ger förutom
+            en matt yta med fantastisk lyster också en giftfri miljö, för både
+            dig och din omvärld.
+          </Col>
+        </Row>
+        <Row style={{ textAlign: "center"}}>
+          <Col span={20} offset={2}>
+            Vår färgskalan bygger på gamla tiders färgsättning och är framtagen
+            med äldre tapeter som förlagor, från sent 1800-tal till senfunkis.
+            Kulörer med naturliga pigment i milda till medelmörka färger. Med
+            den lilla nedtoningeni nyanserna som behövs för att väggarna ska
+            kunna målas i en tydlig kulör utan att bli grälla. Sätt igång, måla
+            vad du vill, väggar och tak, barnens rum, skafferi eller garage, med
+            en modern ekologisk färg.{" "}
+          </Col>
         </Row>
 
         <Row justify="center" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 8 }}
-          >
+          <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
             Produkt 1
           </Col>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 8 }}
-          >
+          <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
             Produkt 2
           </Col>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 8 }}
-          >
+          <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
             Produkt 3
           </Col>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 8 }}
-          >
+          <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
             Produkt 4
           </Col>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 8 }}
-          >
+          <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
             Produkt 5
           </Col>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 8 }}
-          >
+          <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
             Produkt 6
           </Col>
         </Row>
