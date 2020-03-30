5a513614d8e73a773ff6e919d2f84e4b7f9f2dd2
Claudia Aranda <claudia_15_@hotmail.com>
2020-03-30 09:23:26 +0200

    trying to make counter stop at 0

--- src/context.tsx
+++ src/context.tsx
@@ -56,10 +56,10 @@ export class CartProvider extends React.Component<CartProps, CartState> {
      const clonedItems: CartItem[] = Object.assign([], this.state.items)
     // update state
     for (const item of clonedItems) {
-      if (product.artNr === item.product.artNr) {
-        item.quantity-- 
+      if (product.artNr === item.product.artNr && item.quantity < 0 ) {
+       
         this.setState({ items: clonedItems })
-        return
+        return (item.quantity ? item.quantity-- : null)
       }
     }
     
