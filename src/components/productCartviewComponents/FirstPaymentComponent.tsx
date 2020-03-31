9c32eb0019d61636521830c2c622975a7f8c4c48
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-03-31 20:34:58 +0200

    changing to collapse instead of radiobuttons

--- src/components/productCartviewComponents/FirstPaymentComponent.tsx
+++ src/components/productCartviewComponents/FirstPaymentComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Input } from 'antd';
+import { Input, Form, Row, Col  } from 'antd';
 interface Props {
 
 }
@@ -11,10 +11,25 @@ interface State {
 class FirstPaymentComponent extends React.Component<Props, State> {
   render() {
     return (
-        <Input
-        placeholder="hej"
-        suffix="V"
-      />
+      <Row>
+        <Col>
+        <Form style={{
+      display: 'block',
+      height: '2rem',
+      lineHeight: '2rem',
+      width: "100",
+      marginLeft: "10" }}
+    >
+         <Form.Item 
+          label="Success" 
+          hasFeedback validateStatus="success"  
+          help="Should be combination of numbers & alphabets"
+         >
+      <Input placeholder="I'm the content" id="success" />
+    </Form.Item>
+      </Form>
+      </Col>
+      </Row>
     );
   }
 }
--- src/components/productCartviewComponents/PaymentForm.tsx
+++ src/components/productCartviewComponents/PaymentForm.tsx
@@ -1,85 +1,163 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { Radio } from "antd";
-import FirstPaymentComponent from './FirstPaymentComponent'
-import SecondPaymentComponent from './SecondPaymentComponent'
+import { Collapse, Form, Input, InputNumber } from 'antd';
 import ThirdPaymentComponent from './ThirdPaymentComponent'
 
+
 interface Props {
+  
 }
 
 interface State {
 }
 
+const { Panel } = Collapse;
 class PaymentForm extends React.Component<Props, State> {
+  
   state = {
-    value: 1,
+    key: 1,
   };
 
-  onChange = (e:any) => {
-    console.log('radio checked', e.target.value);
-    this.setState({
-      value: e.target.value,
-    });
-  };
-  render() {
-    const radioStyle = {
-      display: 'block',
-      height: '2rem',
-      lineHeight: '2rem',
-    };
-    return (
-
-      <Radio.Group onChange={this.onChange} value={this.state.value}>
-        <Radio style={radioStyle} value={1}>
-          Swish
-          {this.state.value === 1 ? <FirstPaymentComponent /> : null}
-        </Radio>
-        <Radio style={radioStyle} value={2}>
-          Kortbetalning
-          {this.state.value === 2 ? <SecondPaymentComponent /> : null}
-        </Radio>
-        <Radio style={radioStyle} value={3}>
-          Klarna
-          {this.state.value === 3 ? <ThirdPaymentComponent /> : null}
-        </Radio>
-      </Radio.Group>
-      
-    );
+  callback(key:any) {
+    console.log(key);
   }
+  
+  /* 
+  onChange = (e:any) => {
+    console.log('panel checked', e.target.key);
+    this.setState({
+      key: e.target.value,
+    });
+  }; */
+  
+render()
+
+
+ {
+  return (
+    
+<Collapse accordion defaultActiveKey={['1']} onChange={this.callback}>
+    <Panel header="Swish" key="1">
+    <Form >
+         <Form.Item 
+          label="Telefonnummer" 
+          hasFeedback validateStatus="success"  
+          help="Med Swish betalar du din order snabbt och smidigt via din telefon. Kontrollera att telefonnummeret stämmer."
+         >
+      <Input id="Phonenumber" />
+    </Form.Item>
+      </Form>
+    </Panel>
+    <Panel header="Kortbetalning" key="2">
+    <Form>
+    <Form.Item 
+          label="Kortnummer" 
+          hasFeedback validateStatus="success"  
+          help="Du kan betala med ditt VISA, Mastercard eller American Express kort."
+         >
+      <Input id="Phonenumber" />
+    </Form.Item>
+      <Form.Item label="Månad">
+        <Form.Item name="input-number" noStyle>
+          <InputNumber min={1} max={12} />
+        </Form.Item>
+        
+      </Form.Item>
+      <Form.Item label="År">
+        <Form.Item name="input-number" noStyle>
+          <InputNumber min={20} max={34} />
+        </Form.Item>
+       
+      </Form.Item>
+      <Form.Item 
+          label="CVC2" 
+          hasFeedback validateStatus="success"  
+          help="Du finner CVC2-koden på backsidan av ditt bankkort."
+         >
+      <Input id="CVC2" />
+    </Form.Item>
+      </Form>
+    </Panel>
+    <Panel header="Klarna" key="3">
+      <ThirdPaymentComponent />
+    </Panel>
+  </Collapse>
+
+  )}
+
+  }
+  
+  export default PaymentForm;
+  
+
+/* const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+const { Panel } = Collapse;
+
+function callback(key) {
+  console.log(key);
 }
 
+defaultActiveKey={['1']} onChange={callback}
+ */
+/* 
+state = {
+  value: 1,
+};
 
+onChange = (e:any) => {
+  console.log('radio checked', e.target.value);
+  this.setState({
+    value: e.target.value,
+  });
+};
+render() {
+  const radioStyle = {
+    display: 'block',
+    height: '2rem',
+    lineHeight: '2rem',
+  };
+  return (
 
-export default PaymentForm;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <Radio.Group onChange={this.onChange} value={this.state.value}>
+      <Radio style={radioStyle} value={1}>
+        Swish
+        {this.state.value === 1 ? <FirstPaymentComponent /> : null}
+      </Radio>
+      <Radio style={radioStyle} value={2}>
+        Kortbetalning
+        {this.state.value === 2 ? <SecondPaymentComponent /> : null}
+      </Radio>
+      <Radio style={radioStyle} value={3}>
+        Klarna
+        {this.state.value === 3 ? <ThirdPaymentComponent /> : null}
+      </Radio>
+    </Radio.Group>
+    
+  );
+} */
 
 
 
