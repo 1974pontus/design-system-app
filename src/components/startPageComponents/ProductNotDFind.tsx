41383c5331473571b557963278b8e3be80b6e5a7
Claudia Aranda <claudia_15_@hotmail.com>
2020-03-31 11:44:14 +0200

    newComponent for pageNot find

---
+++ src/components/startPageComponents/ProductNotDFind.tsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { Col, Row } from 'antd';
+// import Room from '../img/whiteWall'
+
+
+
+function ProductNotFind() {
+    
+    
+    return (
+        <Row>
+        <Col xs={{ span: 24 }} md={{ span: 20, offset: 2 }}>
+        <img
+       
+        alt="bedroom with a warm yellow painted wall"
+        // src={Room}
+      ></img>
+        </Col>
+      </Row>
+    )
+}
+
+export default ProductNotFind
+
+// const productImage: CSSProperties = {
+//     width: '15rem',
+//     height: '14rem',
+//     objectFit: 'cover'
+//   }
+
+
+  
+ 
