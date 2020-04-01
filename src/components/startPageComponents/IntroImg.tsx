94d328a41e5fb6847d166a7811e5eac5e9ef5ad3
Claudia Aranda <claudia_15_@hotmail.com>
2020-04-01 12:04:32 +0200

    carusellimage smaller

--- src/components/startPageComponents/IntroImg.tsx
+++ src/components/startPageComponents/IntroImg.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties } from "react";
-import { Carousel } from 'antd';
+import { Carousel, Row, Col } from 'antd';
 
 
 interface Props {}
@@ -14,25 +14,37 @@ class IntroImg extends React.Component<Props, State> {
           <div>
         <Carousel autoplay>
         <div>
+        <Row>
+        <Col xs={{ span: 24 }} md={{ span: 20, offset: 2 }}>    
         <img
           style={StartPageImage}
            src="https://www.auro.se/fileadmin/_processed_/csm_Varmgul_Anne_Jeppson_e0200f7f6e.jpg"
           alt="bedroom with a warm yellow painted wall">
           </img>
+          </Col>
+          </Row>
         </div>
         <div>
+        <Row>
+        <Col xs={{ span: 24 }} md={{ span: 20, offset: 2 }}>
         <img
             style={StartPageImage}
             src="https://www.auro.se/fileadmin/filemount/miljobilder/auro/pastellbla.jpg"
             alt="bedroom with a warm yellow painted wall">
             </img>
+        </Col>
+        </Row>
         </div>
         <div>
+        <Row>
+        <Col xs={{ span: 24 }} md={{ span: 20, offset: 2 }}>
         <img
             style={StartPageImage}
             src="https://www.auro.se/fileadmin/filemount/miljobilder/auro/liljevit.jpg"
             alt="bedroom with a warm yellow painted wall">
             </img>
+        </Col>
+        </Row>
         </div>
       </Carousel>,
         
@@ -47,5 +59,5 @@ const StartPageImage: CSSProperties = {
   objectFit: "cover",
   objectPosition: "50% 20%",
   height: "28rem",
-  width: "100vw",
+  width: "100%",
 }
