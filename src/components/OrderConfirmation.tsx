7b7df379a30ad3598be3ff5a8eaa98131aa42c83
Claudia Aranda <claudia_15_@hotmail.com>
2020-04-02 15:08:02 +0200

    OrderConfirmation in MainLayout Switch but is out commented

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -6,6 +6,7 @@ import { CartConsumer, CartItem } from "../context";
 import { Link } from "react-router-dom";
 
 
+
 interface Props {
   cartItem: CartItem;
 }
@@ -22,12 +23,18 @@ class OrderConfirmation extends React.Component<Props, State> {
             <div>
         <Row>
             <Col span={24}>
-                <h1>Tack gör din Colorini beställning</h1>
+                <h1>Mini Colorini</h1>
+                <h3>Tack för att du handlar hos Mini Colorini</h3>
                 <h6>Ditt Ordernummer: </h6>
+                
+                
             </Col>
         </Row>
-
+            <Col span={24}>
+            <h5>Beställningöversikt</h5>
+            </Col>
             <Row gutter={[8, 16]}>
+             
                 <Col span={2}></Col>
                 <Col span={5}>Produkt</Col>
                 <Col span={5}>Antal</Col>
@@ -47,8 +54,10 @@ class OrderConfirmation extends React.Component<Props, State> {
             <Col span={5}>
               <h1>{cartItem.quantity}</h1>
             </Col>
-
+           
+           
             <Col span={5}>{product.price * cartItem.quantity}</Col>
+            <h5>Total</h5>
             <h1>  Total summa inklusive frakt:  {getTotalPriceInclShipper()}kr</h1>
           </Row>
               <Row>
