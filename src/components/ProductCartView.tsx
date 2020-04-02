f1e09cd9047eb0c4256dc8bf2b058df566396b12
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-04-02 14:59:11 +0200

    phone done

--- src/components/ProductCartView.tsx
+++ src/components/ProductCartView.tsx
@@ -13,10 +13,20 @@ interface Props {
   shippingData: ShippingData[]
 }
 interface State {
+  phone: string
 }
 
 class ProductCartView extends React.Component<Props, State> {
-
+  constructor(props:Props) {
+    super(props);
+    this.state= {phone: ""}
+  }
+  
+  onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+    console.log(values.phone)
+    this.setState({phone: values.phone})
+  };
   render() {
     return (
       <CartConsumer>
@@ -25,11 +35,11 @@ class ProductCartView extends React.Component<Props, State> {
               <h1>Kassa</h1>
               <CartForm cartItems={items} />
               <h1>Dina uppgifter</h1>
-              <BillingForm />
+              <BillingForm onFinish={this.onFinish}/>
               <h1>Fraktsätt</h1>
               <ShippingForm shippingData={shippingData}/>
               <h1>Betalsätt</h1>
-              <PaymentForm />
+              <PaymentForm phone={this.state.phone}/>
               <h1>Att betala</h1>
               <TotalCartForm  />
               
