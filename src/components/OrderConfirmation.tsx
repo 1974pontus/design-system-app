ae53e8838bbbc41f3b34e85105cd7810c1530e9f
Claudia Aranda <claudia_15_@hotmail.com>
2020-04-02 13:49:06 +0200

    OrderConfirm Page with right info

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -16,22 +16,33 @@ class OrderConfirmation extends React.Component<Props, State> {
     return (
       <CartConsumer>
         {({ getTotalPriceInclShipper }) => (
+            <div>
+
+            <Row gutter={[8, 16]}>
+                <Col span={2}></Col>
+                <Col span={5}>Produkt</Col>
+                <Col span={5}>Antal</Col>
+                <Col span={5}>Pris</Col>
+         
+            </Row>
+
           <Row gutter={[8, 16]}>
             <Col span={2}>
               <img style={productImage} src={product.productImg} alt={product.alt}/>
             </Col>
-            <Col span={15}>
+
+            <Col span={5}>
                 {product.productName}
             </Col>
-            <Col span={3}>
+
+            <Col span={5}>
               <h1>{cartItem.quantity}</h1>
             </Col>
 
-            <Col span={3}>{product.price * cartItem.quantity}</Col>
-
+            <Col span={5}>{product.price * cartItem.quantity}</Col>
             <h1>  Total summa inklusive frakt:  {getTotalPriceInclShipper()}kr</h1>
-        
           </Row>
+            </div>
         )}
       </CartConsumer>
     );
