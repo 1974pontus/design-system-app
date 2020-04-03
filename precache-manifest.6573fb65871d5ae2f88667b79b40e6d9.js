f19dcbb2a054c1b54111ed0f23ce6b31a5d9fc93
Pontus Tahir <pontus.tahir@gmail.com>
2020-04-03 18:51:01 +0200

    Updates

---
+++ precache-manifest.6573fb65871d5ae2f88667b79b40e6d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4df517881ca86126aaa00b8fa666b4",
+    "url": "/design-system-app/index.html"
+  },
+  {
+    "revision": "24fa0ad1bb7ecfdf1fa8",
+    "url": "/design-system-app/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "e67cdb1f5a1ff9342998",
+    "url": "/design-system-app/static/css/main.b92edf17.chunk.css"
+  },
+  {
+    "revision": "24fa0ad1bb7ecfdf1fa8",
+    "url": "/design-system-app/static/js/2.955329ce.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/design-system-app/static/js/2.955329ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67cdb1f5a1ff9342998",
+    "url": "/design-system-app/static/js/main.792a9b43.chunk.js"
+  },
+  {
+    "revision": "cd1dca184b0dfcbfe376",
+    "url": "/design-system-app/static/js/runtime-main.38befb97.js"
+  },
+  {
+    "revision": "9ca5e463692f83e6eb75906a55026f29",
+    "url": "/design-system-app/static/media/DHL.9ca5e463.jpg"
+  }
+]);
