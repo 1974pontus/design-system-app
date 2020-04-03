42c3400d1800ab8b8adfd51e935e503a929b579f
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-04-03 21:52:27 +0200

    startpage responsive for iphone5

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -18,8 +18,7 @@ class StartPage extends React.Component<Props, State> {
         <Row
           style={{ textAlign: "center", margin: "2rem" }}
           justify="center"
-          gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-        >
+          gutter={{ /* xs: 8, sm: 16, md: 24, lg: 32 */ }}>
           {/* map köllar igeeeenöm igenom alla goa grejer och skapar en go grej. */}
           {productData.map(product => (
             <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
